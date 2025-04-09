f71414032b1e5980a2a04db9b0e20baed0d3ea37
bjornregnell <bjorn.regnell@cs.lth.se>
2025-04-09 17:23:09 +0200

    add 2024, img

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 pandoc -s -c style.css --toc -H header.html --metadata title=SiREN src/index.md -o index.html 
 pandoc -s -c ../style.css -H header.html --metadata title=SiREN2025 src/2025.md -o 2025/index.html 
+pandoc -s -c ../style.css -H header.html --metadata title=SiREN2024 src/2024.md -o 2024/index.html 
